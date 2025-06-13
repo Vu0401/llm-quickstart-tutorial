4bddd7752e878bc7f8435b7ad67a8c655cd78e38
Vu0401 <levu040102@gmail.com>
2025-06-14 00:51:55 +0700

    chore(basic_agent): clean comment

--- llm-quickstart-tutorial/openai_agents_sdk/1_basic_agent.py
+++ llm-quickstart-tutorial/openai_agents_sdk/1_basic_agent.py
@@ -6,9 +6,7 @@ from agents.run import RunConfig
 
 # --- Config ---
 
-
 gemini_api_key = os.getenv("GEMINI_API_KEY")
-print(gemini_api_key)
 
 external_client = AsyncOpenAI(
     api_key=gemini_api_key,
